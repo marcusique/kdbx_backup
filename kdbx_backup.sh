b6115cb2587c22870302949ba039794d9b6dc546
marcusique <marcusique@icloud.com>
2023-03-09 10:35:53 +0100

    fixes to days and log files

--- kdbx_backup.sh
+++ kdbx_backup.sh
@@ -27,7 +27,7 @@ LOGFILE="/var/log/kdbx-backup.log"
 ###################################
 
 echo $(date +%Y-%m-%d_%H-%M-%S) " - Started to zip kdbx files" >>${LOGFILE}
-zip $(date +%Y-%m-%d)_backup.zip ${KDBX_LOCATION}/*.kdbx
+zip $(date +%Y-%m-%d)_backup.zip ${KDBX_LOCATION}/*.kdbx >>${LOGFILE}
 echo $(date +%Y-%m-%d_%H-%M-%S) " - Finished to zip kdbx files "$(date +%Y-%m-%d)_backup.zip >>${LOGFILE}
 
 ##################################################################
@@ -37,7 +37,7 @@ echo $(date +%Y-%m-%d_%H-%M-%S) " - Finished to zip kdbx files "$(date +%Y-%m-%d
 echo $(date +%Y-%m-%d_%H-%M-%S) " - Starting to delete zip files older than 90 days in "${BACKUP_PATH} >>${LOGFILE}
 
 # Remove files older than 7 days
-sudo find ${BACKUP_PATH}/*.zip -mtime +7 -exec rm -r {} \;
+sudo find ${BACKUP_PATH}/*.zip -mtime +90 -exec rm -r {} \; >${LOGFILE}
 
 if [ $? != 0 ]; then
     echo $(date +%Y-%m-%d_%H-%M-%S) " - zip files successfully removed" >>${LOGFILE}
@@ -53,7 +53,7 @@ fi
 
 # Copy the backup to the recent folder
 echo $(date +%Y-%m-%d_%H-%M-%S) " - Copying backup to "${BACKUP_PATH} >>${LOGFILE}
-sudo mv ${KDBX_LOCATION}/$(date +%Y-%m-%d)_backup.zip ${BACKUP_PATH}
+sudo mv ${KDBX_LOCATION}/$(date +%Y-%m-%d)_backup.zip ${BACKUP_PATH} >>${LOGFILE}
 echo $(date +%Y-%m-%d_%H-%M-%S) " - Backup has been copied to "${BACKUP_PATH} >>${LOGFILE}
 
 # Script finished
