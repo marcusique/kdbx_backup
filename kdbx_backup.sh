2bfa014a92b376e787710edd1c0010f994e37ade
marcusique <marcusique@icloud.com>
2023-03-16 10:09:42 +0100

    fixes

--- kdbx_backup.sh
+++ kdbx_backup.sh
@@ -7,8 +7,8 @@ LOGFILE="/var/log/kdbx-backup.log"
 
 echo $(date +%Y-%m-%d_%H-%M-%S) "###### KDBX BACKUP STARTED #####" >>${LOGFILE}
 
-tar -czvf ${BACKUP_PATH}/$(date +%Y-%m-%d)_backup.tar.gz ${KDBX_LOCATION}/*.kdbx --transform 's?.*/??g' >>${LOGFILE}
-echo $(date +%Y-%m-%d_%H-%M-%S) " - Finished to tar.gz kdbx files "$(date +%Y-%m-%d)_backup.tar.gz >>${LOGFILE}
+sudo tar -czvf ${BACKUP_PATH}/$(date +%Y-%m-%d_%H-%M-%S)_backup.tar.gz ${KDBX_LOCATION}/*.kdbx --transform 's?.*/??g' >>${LOGFILE}
+echo $(date +%Y-%m-%d_%H-%M-%S) " - Finished to tar.gz kdbx files "$(date +%Y-%m-%d_%H-%M-%S)_backup.tar.gz >>${LOGFILE}
 
 # Remove files older than 7 days
 sudo find ${BACKUP_PATH}/*.tar.gz -mtime +90 -exec rm -r {} \; >>${LOGFILE}
